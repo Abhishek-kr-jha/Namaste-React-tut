8ec53bc12d9bacee1e4f0292addccafd2bd84f10
abhishekkumarjha <abhishekkjha068@outlook.com>
2023-10-25 02:41:04 +0530

    Episode -2

--- App.js
+++ App.js
@@ -1,21 +1,6 @@
-/**
- * <div id = "parent">
- *    <div id="child">
- *       <h1>I'm h1 tag</h1>
- *        <h2>I'm h1 tag</h2>
- *
- *    </div>
- * <div id="child">
- *       <h1>I'm h1 tag</h1>
- *        <h2>I'm h1 tag</h2>
- *
- *    </div>
- * </div>
- *
- *
- *ReactElement(Object)= HTML(Nrowser understand)
- *
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
@@ -28,16 +13,8 @@ const parent = React.createElement("div", { id: "parent" }, [
   ]),
 ]);
 
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading", xyz: "abc" },
-//   "Hellow World From React!"
-// );
-
-// JSX
-
 console.log(parent); // object
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
+
 root.render(parent);
---
+++ README.md
@@ -0,0 +1,22 @@
+#Namaste React
+
+
+....
+# Parcel
+-Dev Build
+-local server
+-HMR- Hot Module Replacement
+-File Wtching Algorith - written in c++
+-Caching - Faster Builds
+-Image Optimization
+-Minification
+-Bundling
+-Compressing
+-Consistent Hashing
+-Code splitting
+-Differential Bundling -  support older browsers 
+-Diagnostic
+-Error Handling
+-HTTPs
+-Tree Shaking - remove unused code 
+-Different dev and prod bundles
